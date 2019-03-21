d535f65a83c8ba970fc606015f0ffda5cb152111
sjhty <sun_j1986@163.com>
2019-03-21 17:32:34 +0800

    master

---
+++ src/apiConfig/config.js
@@ -0,0 +1,4 @@
+{
+    baseURL: ' https://www.easy-mock.com/mock/5c934cc3e38aea01f54433e1/asdet/';
+    timeout: 1000;
+}
--- src/apiConfig/index.js
+++ src/apiConfig/index.js
@@ -1,2 +1,35 @@
-import Axios from 'axios'
+import axios from 'axios'
 import { Toast } from 'antd-mobile'
+import { baseURL } from '../apiConfig/config'
+
+//添加请求拦截器
+axios.interceptors.request.use( (config) => {
+    //在发送请求之前做些什么
+    return config;
+}, (error) => {
+    //对请求错误做些什么
+    return Promise.reject(error);
+})
+
+//添加响应拦截器
+axios.interceptors.response.use( (response) => {
+    //对响应数据做些什么
+    return response;
+}, (error) => {
+    //对响应错误做些什么
+    return Promise.reject(error);
+})
+
+
+//封装axios请求
+const api = (method, url, params) => {
+    let config = {
+        url: url,
+        method: method,
+        baseURL: baseURL,
+        params: method === 'GET' || method === 'DELETE' ? params : null
+    }
+}
+
+
+
