1c819c21b713477042b2d0f03cdf235540462bf2
sjhty <sun_j1986@163.com>
2019-03-24 15:03:33 +0800

    master

---
+++ src/pages/login/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.less'
+
+export default class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render () {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
